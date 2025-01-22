cf2f0ac3382c1ab9285e9f348544debe0a43eb05
xyinada <ina980765263@gmail.com>
2025-01-22 18:30:14 +0900

    [FIX] fix main.py

--- src/main.py
+++ src/main.py
@@ -10,16 +10,19 @@ import knn
 import ml
 import vis
 
-#name = "supercond_ht"
-for name in ["qct\\bergman",
-             "qct\\mackay", "qct\\tsai"]:
-    print("Target : {}".format(name))
-    settings_path = r"E:\dresearch\material_search_map_property\{}\settings.csv".format(name)
-    settings = data.load_settings(settings_path)
-    
-    data.element_set_train_test_split(settings)
-    knn.gen_rn_training_data(settings)
-    ml.train_all_settings(settings)
-    vis.visualize_test_prediction(settings)
-    vis.gen_heatmap_all(settings)
-    vis.save_vals_all(settings)
+# setting file
+settings = data.load_settings("./example/config/mp_example.json")
+
+# gen dataset
+data.element_set_train_test_split(settings)
+knn.gen_rn_training_data(settings)
+
+# train
+ml.train_all_settings(settings)
+
+# visualize
+vis.visualize_test_prediction(settings)
+vis.gen_heatmap_all(settings)
+
+# save result
+vis.save_vals_all(settings)
